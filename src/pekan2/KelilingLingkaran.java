7037fe1bf1f1b3bd3e4bc5a0997225d0e3087584
JoseAze <josemonizdearouju@gmail.com>
2025-09-24 17:00:19 +0700

    Add files via upload

---
+++ src/pekan2/KelilingLingkaran.java
@@ -0,0 +1,13 @@
+package pekan2;
+
+public class KelilingLingkaran {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	final double PI = 3.14; /* Definisi Konstanta */
+	double radius = 30; /* Deklarasi Variable */
+	System.out.println("keliling = "+ 2 * PI * radius);
+
+	}
+
+}
