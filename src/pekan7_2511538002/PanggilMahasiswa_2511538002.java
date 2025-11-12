7b6c8b874e66e10422a369670e43826faf29a6d1
JoseAze <josemonizdearouju@gmail.com>
2025-11-12 15:11:53 +0700

    Add files via upload

---
+++ src/pekan7_2511538002/PanggilMahasiswa_2511538002.java
@@ -0,0 +1,14 @@
+package pekan7_2511538002;
+
+public class PanggilMahasiswa_2511538002 {
+
+	public static void main(String[] args) {
+		Mahasiswa_2511538002 a = new Mahasiswa_2511538002();
+		a.setNim(8002);
+		a.setNama("Rhamat");
+		System.out.println(a.getNim());
+		System.out.println(a.getNama());
+		a.cetak();
+	}
+
+}
